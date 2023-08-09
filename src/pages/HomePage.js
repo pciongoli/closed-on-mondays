663dc758281b0d2b256dd42ace2af634c793dd81
pcion <patrick.ciongoli@gmail.com>
2023-08-08 17:03:09 -0700

    minor updates to homepage and portfoliopage

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -5,7 +5,32 @@ import "../styles/HomePage.css";
 const HomePage = () => (
    <div className="home">
       <h1>Welcome to Closed On Mondays</h1>
-      <p>Your destination for professional video editing services.</p>
+      <h2 className="introduction">
+         Your destination for professional video editing services.
+      </h2>
+
+      <div className="showcase">
+         <h2>Showcase</h2>
+         <video controls>
+            <source src="showcase.mp4" type="video/mp4" />
+            Your browser does not support the video tag.
+         </video>
+      </div>
+
+      <div className="cta-buttons">
+         <button onClick={() => (window.location.href = "/services")}>
+            View Services
+         </button>
+         <button onClick={() => (window.location.href = "/contact")}>
+            Get in Touch
+         </button>
+      </div>
+
+      <div className="services-overview">
+         <h2>Services Overview</h2>
+         <p>We offer a wide range of video editing services, including...</p>
+         {/* You can add more details or list the services here */}
+      </div>
    </div>
 );
 
--- src/pages/PortfolioPage.js
+++ src/pages/PortfolioPage.js
@@ -5,7 +5,26 @@ import "../styles/PortfolioPage.css";
 const PortfolioPage = () => (
    <div className="portfolio">
       <h1>Portfolio</h1>
-      <p>Here you'll find a collection of my work...</p>
+      <div className="projects">
+         {/* Example project */}
+         <div className="project">
+            <div className="video-preview">
+               <video controls>
+                  <source src="project1.mp4" type="video/mp4" />
+                  Your browser does not support the video tag.
+               </video>
+            </div>
+            <h3 className="project-title">Project Title 1</h3>
+            <p className="project-details">
+               Description and details about the project...
+            </p>
+            <p className="technical-details">
+               Technical details, software used, etc...
+            </p>
+         </div>
+
+         {/* You can add more projects similarly */}
+      </div>
    </div>
 );
 
